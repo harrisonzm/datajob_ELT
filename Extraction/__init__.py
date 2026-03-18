bb603f67ce183f03b538bf81cb013b46a620e476
Harrison Zuleta Montoya <harrison.zmontoya@gmail.com>
2026-03-18 02:24:02 -0500

    chore: remove unused files

--- Extraction/__init__.py
+++ Extraction/__init__.py
@@ -1,1 +1,5 @@
-"""Extraction package"""
+"""Extraction package for ETL pipeline"""
+
+from .extraction import execute_extraction, load_optimized_fast, load_with_copy
+
+__all__ = ['execute_extraction', 'load_optimized_fast', 'load_with_copy']
--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,5 @@
-"""Database package"""
+"""Database package for ETL pipeline"""
+
+from .config.db import engine, create_tables, drop_tables, get_db
+
+__all__ = ['engine', 'create_tables', 'drop_tables', 'get_db']
